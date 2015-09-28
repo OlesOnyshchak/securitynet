6f81adb85b1bbb92d25ea04c3c40342beae539a3
Oles Onyshchak <oles1719@gmail.com>
2015-09-28 19:58:21 +0300

    add save person opportunity

--- web/src/main/java/com/softserve/edu/controller/PersonController.java
+++ web/src/main/java/com/softserve/edu/controller/PersonController.java
@@ -2,8 +2,12 @@ package com.softserve.edu.controller;
 
 import com.softserve.edu.dto.PersonDTO;
 import com.softserve.edu.service.PersonService;
+import entity.Person;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/")
@@ -12,8 +16,15 @@ public class PersonController {
     @Autowired
     PersonService personService;
 
-    @RequestMapping(value = "save-person", method = RequestMethod.POST)
-    public void getNewUser(@RequestBody PersonDTO personDTO) {
-        System.out.println(personDTO);
+    @RequestMapping(value = "{save-person}", method = RequestMethod.POST )
+    public PersonDTO getNewUser(@RequestBody PersonDTO personDTO) {
+        personService.save(
+                            new Person(
+                                    personDTO.getFirstName(),
+                                    personDTO.getLastName(),
+                                    personDTO.getDateOfBirth()
+                                    )
+                             );
+        return personDTO;
     }
 }
