ff3d303b1dd4518ec5b54fb360280c78472a8959
Oles Onyshchak <oles1719@gmail.com>
2015-09-28 18:39:28 +0300

    add RestController

--- web/src/main/java/com/softserve/edu/controller/PersonController.java
+++ web/src/main/java/com/softserve/edu/controller/PersonController.java
@@ -1,12 +1,19 @@
 package com.softserve.edu.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.softserve.edu.dto.PersonDTO;
+import com.softserve.edu.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@RequestMapping(value = "/")
 public class PersonController {
-    @RequestMapping(value = {"/"})
-    public String showWelcomePage() {
-        return "app/index.html";
+
+    @Autowired
+    PersonService personService;
+
+    @RequestMapping(value = "save-person", method = RequestMethod.POST)
+    public void getNewUser(@RequestBody PersonDTO personDTO) {
+        System.out.println(personDTO);
     }
 }
